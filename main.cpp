0acb9f184f6ceb8e6f55764e9b6774a2a417fd1a
Mario Pardo <mario.pardo003@gmail.com>
2024-03-28 14:16:08 -0400

    removed main from pr

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include "mainmenu.h"
-#include "sessionlogwindow.h"
-#include "pcwindow.h"
-
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    PCWindow w;
+    MainMenu w;
     w.show();
     return a.exec();
 }
