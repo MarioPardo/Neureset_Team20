a8244b46228d0a70d0a1e35df181e7eac96224ef
AnhTu Vothang <100483933+anhtuv@users.noreply.github.com>
2024-04-15 18:52:15 -0400

    Update datetimewindow.cpp

--- datetimewindow.cpp
+++ datetimewindow.cpp
@@ -9,11 +9,6 @@ datetimewindow::datetimewindow(QWidget *parent) :
     ui->setupUi(this);
 
     dateTimeEdit = ui->dateTimeEdit;
-    //QDateTime DT = ui->dateTimeEdit->dateTime();
-    // connect(ui->dateTimeEdit, &QDateTimeEdit::dateTimeChanged, [&](const QDateTime &dateTime) {
-    //    qDebug() << "Date and time changed to: " << dateTime.toString();
-    //});
-    //connect(ui->dateTimeEdit, &QDateTimeEdit::dateTimeChanged, this, &datetimewindow::ondateTimeChanged);
     connect(dateTimeEdit, &QDateTimeEdit::dateTimeChanged, this, &datetimewindow::onDateTimeChanged);
 }
 
@@ -24,13 +19,11 @@ datetimewindow::~datetimewindow()
 
 
 QDateTime datetimewindow::getDateTimeValue() const {
-    //return dateTimeValue;
     return dateTimeEdit->dateTime();
 }
 
 
 void datetimewindow::onDateTimeChanged(const QDateTime &dateTime){
-    //dateTimeValue = dateTime;
     emit dateTimeValueChanged(dateTime);
     qDebug() << "Selected Date and Time: " << dateTime.toString("yyyy-MM-dd hh:mm:ss");
 }
